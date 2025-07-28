e403aa8110a694532bcacaa9a3c6c96225583ae7
新冬 夏 <2504251661@qq.com>
2025-07-28 17:12:15 +0800

    匈牙利求二分图最大匹配

---
+++ 匈牙利求二分图的最大匹配/main.c
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+#define N 510
+#define M 100010
+
+int h[N], e[M], ne[M], idx;
+bool st[N];
+int match[N];
+int n1, n2, m;
+
+void add(int a, int b)
+{
+	e[idx] = b;
+	ne[idx] = h[a];
+	h[a] = idx;
+	idx++;
+}
+
+bool find(int x)
+{
+	for (int i = h[x]; i != -1; i = ne[i])
+	{
+		int j = e[i];
+		if (!st[j])
+		{
+			st[j] = true;
+			if (match[j] == 0 || find(match[j]))
+			{
+				match[j] = x;
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+int main(void)
+{
+	memset(h, -1, sizeof h);
+	scanf("%d%d%d", &n1, &n2, &m);
+
+	while (m--)
+	{
+		int a, b;
+		scanf("%d%d", &a, &b);
+
+		add(a, b);
+	}
+
+	int res = 0;
+	for (int i = 1; i <= n1; i++)
+	{
+		memset(st, false, sizeof st);
+		if (find(i)) res++;
+	}
+
+	printf("%d", res);
+
+	return 0;
+}
